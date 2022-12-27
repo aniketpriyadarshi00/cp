9887b3750c5a8a1f74e869240e9df15fdab758bf
Aniket Priyadarshi <77151836+aniketpriyadarshi00@users.noreply.github.com>
2022-12-27 09:24:27 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define endl '\n'
+#define gap ' '
+vector<long long> sieve(int n) {int*arr = new int[n + 1](); vector<long long> vect; for (int i = 2; i <= n; i++)if (arr[i] == 0) {vect.push_back(i); for (int j = 2 * i; j <= n; j += i)arr[j] = 1;} return vect;}
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+void __print(int x) {cout << x;}
+void __print(long x) {cout << x;}
+void __print(long long x) {cout << x;}
+void __print(unsigned x) {cout << x;}
+void __print(unsigned long x) {cout << x;}
+void __print(unsigned long long x) {cout << x;}
+void __print(float x) {cout << x;}
+void __print(double x) {cout << x;}
+void __print(long double x) {cout << x;}
+void __print(char x) {cout << "\'" << x << "\'";}
+void __print(const char *x) {cout << '"' << x << '"';}
+void __print(const string &x) {cout << '"' << x << '"';}
+void __print(bool x) {cout << (x ? "true" : "false");}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cout << '{'; __print(x.first); cout << ','; __print(x.second); cout << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cout << '{'; for (auto &i: x) cout << (f++ ? "," : ""), __print(i); cout << "}";}
+void _print() {cout << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cout << ", "; _print(v...);}
+#ifdef DEB
+#define debug(x...) cout << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+int main() {
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+
+  ;
+
+  return 0;
+}
